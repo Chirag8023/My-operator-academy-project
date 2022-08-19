23a1a36c1e85c28b5bbfd4b5fd4eff22593802bf
Chirag Arora <chirag8023@gmail.com>
2022-08-19 18:44:01 +0530

    project complete

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-// Import express and the file containing our route definitions
 const express = require("express");
+const path = require('path');
+const bodyparser = require("body-parser");
+const session = require("express-session");
+const {v4: uuidv4} = require("uuid");
+const router = require('./router');
+
 const app = express();
-const routes = require("./src/routes");
-
-// Import Twilio and initialize the client.
-// IMPORTANT: Remember to set environment variables for your Account SID and Auth Token.
-const twilio = require("twilio");
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
-
-// Configure the express application
 const port = 3000;
-app.use(express.urlencoded({ extended: false }));
-app.use("/", routes);
 
-// Start the server
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended : true}));
+
+app.set('view engine','ejs');
+
+//load static assests
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
+app.use(session({
+  secret: uuidv4(),
+  resave: false,
+  saveUninitialize: true,
+}));
+
+app.use('/route', router);
+
+
+//home route
+app.get("/",(req, res) => {
+  res.render('base', {title:"Login System"});
+})
+
 app.listen(port, () => {
-  /*
-  This is an example of string interpolation via Template Literals.
-  This is an easier way of building strings than we've seen in the JS Test Lab. Read more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-  */
-  console.log(`Your Express application is running on port ${port}`);
+  console.log(`your app is running on port: http://localhost:${port} `)
 });
+
+
